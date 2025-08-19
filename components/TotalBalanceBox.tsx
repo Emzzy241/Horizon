d84784288bcd5bd60a18db414f1e81a8d8abeee3
Emzzy241 <emzzyoluwole@gmail.com>
2025-08-19 17:58:41 +0100

    update TotalBalanceBox component with tailwing stylings

--- components/TotalBalanceBox.tsx
+++ components/TotalBalanceBox.tsx
@@ -5,29 +5,29 @@ import DoughnutChart from './DoughnutChart'
 const TotalBalanceBox = ({
     accounts = [], totalBanks, totalCurrentBalance
 }: TotalBalanceBoxProps) => {
-  return (
-      <section className='total-balance'>
-        <div className='total-balance-chart'>
-            {/* <DoughnutChart accounts={accounts} /> */}
-            <DoughnutChart accounts={accounts} />
-        </div>
+    return (
+        <section className='flex w-full items-center gap-4 rounded-xl border border-gray-200 p-4 shadow-chart sm:gap-6 sm:p-6;'>
+            <div className='flex size-full max-w-[100px] items-center sm:max-w-[120px]'>
+                {/* <DoughnutChart accounts={accounts} /> */}
+                <DoughnutChart accounts={accounts} />
+            </div>
 
-        <div className='flex flex-col gap-6'>
-            <h2 className='header-2'>
-                Bank Accounts: {totalBanks} 
+            <div className='flex flex-col gap-6'>
+                <h2 className='text-xl font-semibold text-gray-900;'>
+                    Bank Accounts: {totalBanks}
                 </h2>
                 <div className="flex flex-col gap-2">
-                    <p className='total-balance-label'>
+                    <p className='text-base font-medium text-gray-600'>
                         Total Current Balance
                     </p>
 
-                    <div className="total-balance-amount flex-center gap-2">
+                    <div className="text-xl lg:text-30 flex-1 font-semibold text-gray-900 flex-center gap-2">
                         <AnimatedCounter amount={totalCurrentBalance} />
                     </div>
                 </div>
-        </div>
-    </section>
-  )
+            </div>
+        </section>
+    )
 }
 
 export default TotalBalanceBox
